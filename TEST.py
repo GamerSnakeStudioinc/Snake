e53527c9eaaf3193bcc77f3130b3b52d1ada9749
GamerSnakeStudioinc <102850055+GamerSnakeStudioinc@users.noreply.github.com>
2022-04-05 12:57:20 +0700

    Reset

--- TEST.py
+++ TEST.py
@@ -1,7 +1,15 @@
 import pygame
 
 pygame.init()
-
+class pos:
+    def __init__(self, x, y):
+        self.pos = x, y
+        self.pos_x = x
+        self.pos_y = y
+    def set_pos(self, new_x, new_y):
+        self.pos = new_x, new_y
+    def print_pos(self):
+        print("Position: %s, %s" % self.pos)
 W_OF_SCREEN, H_OF_SCREEN = 600, 400
 
 sc = pygame.display.set_mode((W_OF_SCREEN, H_OF_SCREEN))
@@ -23,5 +31,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
+    food_pos = pos(0, 0)
+    food_pos.set_pos(19, 19)
+    food_pos.print_pos()
+    food = pygame.Rect((food_pos.pos_x, food_pos.pos_y), 10, 10)
+    pygame.draw.rect(sc, RED, food)
+    pygame.display.update()
     clock.tick(FPS)
